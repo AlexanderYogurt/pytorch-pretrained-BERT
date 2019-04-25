d93b3ab4d15321d7e758cf2e6a180c39eb6423bc
Ubuntu <ubuntu@ip-172-31-3-186.ec2.internal>
2019-04-25 10:42:15 +0000

    test set

--- examples/run_classifier.py
+++ examples/run_classifier.py
@@ -147,8 +147,8 @@ class SnliProcessor(DataProcessor):
     def get_dev_examples(self, data_dir):
         """See base class."""
         return self._create_examples(
-            self._read_tsv(os.path.join(data_dir, "dev.tsv")),
-            "dev_matched")
+            self._read_tsv(os.path.join(data_dir, "test.tsv")),
+            "dev")
 
     def get_labels(self):
         """See base class."""
